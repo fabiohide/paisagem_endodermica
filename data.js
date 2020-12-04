392e9e03940291394e06ffbc7c6d45360cd02024
Fábio Hideki <59976172+fabiohide@users.noreply.github.com>
2020-12-03 22:17:50 -0300

    Commit inicial

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "•",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -0.03379915338722306,
+        "pitch": 0.0023668294052630046,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8325045636050419,
+          "pitch": 0.2434106859295575,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.0657290134389257,
+          "pitch": 0.1900243589057773,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "••",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.341603289483824
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.037539289734454,
+          "pitch": 0.2473034791630102,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.1089323799611144,
+          "pitch": 0.5393996648023442,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "•••",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3415920490266495
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0203788316538684,
+          "pitch": 0.2640472848401423,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.290574648233159,
+          "pitch": 0.35454717202648744,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Giovana Macedo - paisagem endodérmica",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
